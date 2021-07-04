526963e1393575d45af2195fe25c040462ec0e8c
nextdoorwith <nextdoorwithadm@gmail.com>
2021-07-04 11:20:36 +0900

    add ExampleUtils

---
+++ example-console/ExampleUtils/src/Utils/NumberUtils.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ExampleUtils.Utils
+{
+    /// <summary>
+    /// 数値ユーティリティ
+    /// </summary>
+    public static class NumberUtils
+    {
+        /// <summary>
+        /// 上位4ビットを保持するマスク
+        /// </summary>
+        private const byte Upper4Mask = 0b_1111_0000;
+
+        /// <summary>
+        /// 下位4ビットを保持するマスク
+        /// </summary>
+        private const byte Lower4Mask = 0b_0000_1111;
+
+        /// <summary>
+        /// ゾーン10進数: ゾーン部(ASCIIコード使用時)
+        /// </summary>
+        private const char ZonedDecAsciiZonePart = '3'; // 0b_0011_0000
+
+        /// <summary>
+        /// ゾーン10進数: 符号部 符号なし・正(ASCIIコード使用時)
+        /// </summary>
+        private const char ZonedDecAsciiSignPartPlus = '3'; // 0b_0011_0000
+
+        /// <summary>
+        /// ゾーン10進数: 符号部 負(ASCIIコード使用時)
+        /// </summary>
+        private const char ZonedDecAsciiSignPartMinus = '7'; // 0b_0111_0000
+
+        /// <summary>
+        /// パック10進数: 符号部 正(ASCIIコード使用時)
+        /// </summary>
+        private const char PackedDecSignPartPlus = 'C';
+
+        /// <summary>
+        /// パック10進数: 符号部 負(ASCIIコード使用時)
+        /// </summary>
+        private const char PackedDecSignPartMinus = 'D';
+
+        /// <summary>
+        /// 整数からゾーン10進数を生成する。
+        /// </summary>
+        /// <param name="num">整数</param>
+        /// <returns>ゾーン10進数</returns>
+        public static byte[] CreateZonedDecimalBytes(long num)
+        {
+            var isPositive = num >= 0;
+            var numstr = num.ToString().TrimStart('-'); // -123 => 123
+            var sb = new StringBuilder();
+
+            // 最後を除く16進数2桁の生成
+            foreach (var c in numstr[0..^1])
+                sb.Append(ZonedDecAsciiZonePart).Append(c);
+
+            // 最後の16進数2桁の生成
+            var sign = isPositive ? ZonedDecAsciiSignPartPlus : ZonedDecAsciiSignPartMinus;
+            sb.Append(sign).Append(numstr[^1]);
+
+            // 文字列の各2桁を16進数としてバイトに変換する。
+            var str = sb.ToString();
+            return Enumerable.Range(0, str.Length)
+                .Where(i => i % 2 == 0)
+                .Select(i => Convert.ToByte(str.Substring(i, 2), 16))
+                .ToArray();
+        }
+
+        /// <summary>
+        /// ゾーン10進数から整数を生成する。
+        /// </summary>
+        /// <param name="bytes">ゾーン10進数</param>
+        /// <returns>整数</returns>
+        public static long ParseZonedDecimal(byte[] bytes)
+        {
+            var hexstr = BitConverter.ToString(bytes).Replace("-", ""); // 1F-34 => 1F34
+            var sb = new StringBuilder();
+            for (var i = 0; i < hexstr.Length; i+=2)
+            {
+                // 上位4ビット(ゾーン部or符号部)の検証
+                var u4 = hexstr[i];
+                if (i < hexstr.Length - 2)
+                {
+                    // 最終バイト以外は期待するゾーン部であることを検証
+                    if (u4 != ZonedDecAsciiZonePart)
+                        throw new ArgumentException($"不正なゾーン部: {hexstr}");
+                }
+                else
+                {
+                    // 最終バイトは符号部であることを検証
+                    if (u4 == ZonedDecAsciiSignPartMinus)
+                        sb.Insert(0, "-");
+                    else if (u4 != ZonedDecAsciiSignPartPlus)
+                        throw new ArgumentException($"不正な符号部: {hexstr}");
+                }
+
+                // 下位4ビットは数値(0-9)であることを検証
+                var l4 = hexstr[i + 1];
+                if (l4 < '0' || '9' < l4)
+                    throw new ArgumentException($"不正な数値部: {hexstr}");
+
+                sb.Append(l4);
+            }
+            return long.Parse(sb.ToString()); // OverflowExceptionの場合あり
+        }
+
+        /// <summary>
+        /// 整数からパック10進数を生成する。
+        /// </summary>
+        /// <param name="num">整数</param>
+        /// <returns>パック10進数</returns>
+        public static byte[] CreatePackedDecimalBytes(long num)
+        {
+            var isPositive = num >= 0;
+            var sb = new StringBuilder(num.ToString().TrimStart('-')); // -123 => 123
+
+            // 偶数桁の16進数文字列になるよう0を追加
+            // (後で符号部1桁を追加するので、ここでは奇数である必要がある。)
+            if (sb.Length % 2 == 0) sb.Insert(0, '0');
+
+            // 符号部1桁を追加(16進数)
+            sb.Append(isPositive ? PackedDecSignPartPlus : PackedDecSignPartMinus);
+
+            // 文字列の各2桁を16進数としてバイトに変換する。
+            var str = sb.ToString();
+            return Enumerable.Range(0, str.Length)
+                .Where(i => i % 2 == 0)
+                .Select(i => Convert.ToByte(str.Substring(i, 2), 16))
+                .ToArray();
+        }
+
+        /// <summary>
+        /// パック10進数から整数を生成する。
+        /// </summary>
+        /// <param name="bytes">パック10進数</param>
+        /// <returns>整数</returns>
+        public static long ParsePackedDecimal(byte[] bytes)
+        {
+            // 16進数文字列を生成
+            var hexes = BitConverter.ToString(bytes).Replace("-", ""); // 1F-34 => 1F34
+
+            // 数値以外(a-f)が含まれないことを検証
+            var numstr = hexes[0..^1];
+            foreach (var c in numstr)
+                if (c < '0' || '9' < c) throw new ArgumentException($"不正な数値部: {hexes}");
+
+            // 符号部が想定される符号値であることを検証
+            var signstr = hexes[^1];
+            if (signstr == PackedDecSignPartMinus)
+                numstr = "-" + numstr;
+            else if (signstr != PackedDecSignPartPlus)
+                throw new ArgumentException($"不正な符号部: {hexes}");
+
+            return long.Parse(numstr); // OverflowExceptionの場合あり
+        }
+    }
+
+}
---
+++ example-console/ExampleUtils/test/Utils/NumberUtilsTests.cs
@@ -0,0 +1,117 @@
+﻿using ExampleUtils.Utils;
+using System;
+using Xunit;
+
+namespace ExampleUtils.Test.Utils
+{
+    public class NumberUtilsTests
+    {
+        // CreateZonedDecimalBytes
+
+        [Theory(DisplayName = "整数からゾーン10進数を生成: 正常系")]
+        [InlineData(new byte[] { 0x30 }, 0)]
+        [InlineData(new byte[] { 0x71 }, -1)]
+        [InlineData(new byte[] { 0x31, 0x32, 0x33 }, 123)]
+        [InlineData(new byte[] { 0x31, 0x32, 0x73 }, -123)]
+        public void Test_CreateZonedDecimalBytes_Ok(byte[] expected, long test)
+            => Assert.Equal(expected, NumberUtils.CreateZonedDecimalBytes(test));
+
+        // ParseZonedDecimal
+
+        [Theory(DisplayName = "ゾーン10進数から整数を生成: 正常系")]
+        [InlineData(0, new byte[] { 0x30 })]
+        [InlineData(-1, new byte[] { 0x71 })]
+        [InlineData(123, new byte[] { 0x31, 0x32, 0x33 })]
+        [InlineData(-123, new byte[] { 0x31, 0x32, 0x73 })]
+        public void Test_ParseZonedDecimal_Ok(long expected, byte[] test)
+            => Assert.Equal(expected, NumberUtils.ParseZonedDecimal(test));
+
+        [Theory(DisplayName = "ゾーン10進数から整数を生成: 異常系(ゾーン部不正)")]
+        [InlineData(new byte[] { 0x31, 0x42, 0x33 })]
+        public void Test_ParseZonedDecimal_Ng_InvalidZonePart(byte[] test)
+        {
+            var ex = Assert.Throws<ArgumentException>( ()=> NumberUtils.ParseZonedDecimal(test));
+            Assert.StartsWith("不正なゾーン部: ", ex.Message);
+        }
+
+        [Theory(DisplayName = "ゾーン10進数から整数を生成: 異常系(符号部不正)")]
+        [InlineData(new byte[] { 0x31, 0x32, 0x43 })]
+        public void Test_ParseZonedDecimal_Ng_InvalidSignPart(byte[] test)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => NumberUtils.ParseZonedDecimal(test));
+            Assert.StartsWith("不正な符号部: ", ex.Message);
+        }
+
+        [Theory(DisplayName = "ゾーン10進数から整数を生成: 異常系(数値部不正)")]
+        [InlineData(new byte[] { 0x31, 0x3a, 0x33 })]
+        [InlineData(new byte[] { 0x31, 0x3a, 0x3b })]
+        public void Test_ParseZonedDecimal_Ng_InvalidNumPart(byte[] test)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => NumberUtils.ParseZonedDecimal(test));
+            Assert.StartsWith("不正な数値部: ", ex.Message);
+        }
+
+        [Fact(DisplayName = "ゾーン10進数から整数を生成: 異常系(桁あふれ)")]
+        public void Test_ParseZonedDecimal_Ng_Overflow()
+        {
+            var test = new byte[] {
+                0x39, 0x39, 0x39, 0x39, 0x39, /* */ 0x39, 0x39, 0x39, 0x39, 0x39,
+                0x39, 0x39, 0x39, 0x39, 0x39, /* */ 0x39, 0x39, 0x39, 0x39, 0x39
+            };
+            var ex = Assert.Throws<OverflowException>(() => 
+                NumberUtils.ParseZonedDecimal(test));
+            Assert.Equal("Value was either too large or too small for an Int64.", ex.Message);
+        }
+
+        // CreatePackedDecimalBytes
+
+        [Theory(DisplayName = "整数からパック10進数を生成: 正常系")]
+        [InlineData(new byte[] { 0x0c }, 0)]
+        [InlineData(new byte[] { 0x1d }, -1)]
+        [InlineData(new byte[] { 0x12, 0x3c }, 123)]
+        [InlineData(new byte[] { 0x12, 0x3d }, -123)]
+        public void Test_CreatePackedDecimalBytes_Ok(byte[] expected, long test)
+            => Assert.Equal(expected, NumberUtils.CreatePackedDecimalBytes(test));
+
+        // ParsePackedDecimal
+
+        [Theory(DisplayName = "パック10進数から整数を生成: 正常系")]
+        [InlineData(0, new byte[] { 0x0c })]
+        [InlineData(-1, new byte[] { 0x1d })]
+        [InlineData(123, new byte[] { 0x12, 0x3c })]
+        [InlineData(-123, new byte[] { 0x12, 0x3d })]
+        public void Test_ParsePackedDecimal_Ok(long expected, byte[] test)
+            => Assert.Equal(expected, NumberUtils.ParsePackedDecimal(test));
+
+        [Theory(DisplayName = "パック10進数から整数を生成: 異常系(符号部不正)")]
+        [InlineData(new byte[] { 0x01, 0x2f })]
+        public void Test_ParsePackedDecimal_Ng_InvalidSignPart(byte[] test)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => NumberUtils.ParsePackedDecimal(test));
+            Assert.StartsWith("不正な符号部: ", ex.Message);
+        }
+
+        [Theory(DisplayName = "パック10進数から整数を生成: 異常系(数値部不正)")]
+        [InlineData(new byte[] { 0xa2, 0x3c })]
+        [InlineData(new byte[] { 0x1b, 0x3c })]
+        [InlineData(new byte[] { 0x12, 0xcc })]
+        public void Test_ParsePackedDecimal_Ng_InvalidNumPart(byte[] test)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => NumberUtils.ParsePackedDecimal(test));
+            Assert.StartsWith("不正な数値部: ", ex.Message);
+        }
+
+        [Fact(DisplayName = "パック10進数から整数を生成: 異常系(桁あふれ)")]
+        public void Test_ParsePackedDecimal_Ng_Overflow()
+        {
+            var test = new byte[] {
+                0x99, 0x99, 0x99, 0x99, 0x99, /* */ 0x99, 0x99, 0x99, 0x99, 0x9d
+            };
+            var ex = Assert.Throws<OverflowException>(() =>
+                NumberUtils.ParsePackedDecimal(test));
+            Assert.Equal("Value was either too large or too small for an Int64.", ex.Message);
+        }
+
+    }
+
+}
