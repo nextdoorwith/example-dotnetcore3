12dc3b72ade7f4ede5baa64c979adbb405d857e3
nextdoorwith <nextdoorwithadm@gmail.com>
2021-12-05 00:51:51 +0900

    add Moq examples

--- example-console/BasicExample/XunitExamples/MockExamples.cs
+++ example-console/BasicExample/XunitExamples/MockExamples.cs
@@ -11,8 +11,12 @@ namespace BasicExample.XunitExamples
         [Fact(DisplayName = "基本的なモック")]
         public async Task BasicMock()
         {
+            // モックオブジェクトを生成
             var targetMock = new Mock<ITarget>();
             ITarget target = targetMock.Object;
+
+            // モック化対象のオブジェクトを直接取得も可
+            //ITarget target = Mock.Of<ITarget>();
 
             // 特定引数に対して固定値を返却するモック
             // モック対象: public int Add(int x, int y)
@@ -127,6 +131,45 @@ namespace BasicExample.XunitExamples
             Assert.Throws<InvalidOperationException>(() => target.Increment());
         }
 
+        [Fact(DisplayName = "プロパティのモック(値の保持可能)")]
+        public void PropertyMock()
+        {
+            var targetMock = new Mock<ITarget>();
+            ITarget target = targetMock.Object;
+
+            // 値を保持可能なプロパティの実装
+            // ("o = > o.Data1.Data2.Value"等のようにネストしたプロパティの定義も可)
+            targetMock.SetupProperty(o => o.StrProp);    // Get+Set実装
+            targetMock.SetupProperty(o => o.IntProp, 1); // Get+Set実装、初期値設定
+
+            // テスト
+            Assert.Null(target.StrProp);
+            target.StrProp = "test";
+            Assert.Equal("test", target.StrProp);
+            // テスト
+            Assert.Equal(1, target.IntProp);
+            target.IntProp = 2;
+            Assert.Equal(2, target.IntProp);
+        }
+
+        [Fact(DisplayName = "読み取り専用プロパティのモック")]
+        public void ReadonlyPropertyMock()
+        {
+            var targetMock = new Mock<ITarget>();
+            ITarget target = targetMock.Object;
+
+            // setがないプロパティにSetupProperty()するとArgumentException
+            // ("Property ITarget.ReadonlyStrProp does not have a setter.")
+            // targetMock.SetupProperty(o => o.ReadonlyStrProp, "test");
+
+            targetMock.Setup(o => o.ReadonlyStrProp).Returns("test"); // 例１
+            targetMock.SetupGet(o => o.ReadonlyIntProp).Returns(1);   // 例２
+
+            // テスト
+            Assert.Equal("test", target.ReadonlyStrProp);
+            Assert.Equal(1, target.ReadonlyIntProp);
+        }
+
         [Fact(DisplayName = "外部条件に基づいた値を返却するモック")]
         public void ExternalConditionMock()
         {
@@ -144,8 +187,8 @@ namespace BasicExample.XunitExamples
             region = "en"; Assert.Equal("1-1", target.GetDate());
         }
 
-        [Fact(DisplayName = "戻り値なしモックとその検証(1)")]
-        public void NoReturnMockAndAssert()
+        [Fact(DisplayName = "voidモックとその検証(1)")]
+        public void VoidAndAssert()
         {
             var targetMock = new Mock<ITarget>();
             ITarget target = targetMock.Object;
@@ -169,7 +212,7 @@ namespace BasicExample.XunitExamples
         }
 
         [Fact(DisplayName = "戻り値なしモックとその検証(2)")]
-        public void InternalMockExample()
+        public void VoidAndAssert2()
         {
             // モック呼び出し時の引数を保持
             var sentMessages = new List<string>();
@@ -190,11 +233,149 @@ namespace BasicExample.XunitExamples
             Assert.Equal("message2", sentMessages[1]);
         }
 
+        [Fact(DisplayName = "モック作成比較1")]
+        public void NewMockVsMockOfExample1()
+        {
+            var target1Mock = new Mock<ITarget>();
+            ITarget target1 = target1Mock.Object;
+
+            ITarget target2 = Mock.Of<ITarget>();
+
+            target1.StrProp = "test";
+            Assert.Null(target1.StrProp); // 既定ではプロパティ値が保持されない
+
+            target2.StrProp = "test";
+            Assert.Equal("test", target2.StrProp);
+        }
+
+        [Fact(DisplayName = "モック作成比較2")]
+        public void NewMockVsMockOfExample2()
+        {
+            // new Mock<T>()によるプロパティ・メソッドのモック化
+            var target1Mock = new Mock<ITarget>();
+            target1Mock.Setup(o => o.StrProp).Returns("test");
+            target1Mock.Setup(o => o.Add(2, 3)).Returns(5);
+            ITarget target1 = target1Mock.Object;
+
+            // Mock.Of<T>()によるプロパティ・メソッドのモック化(LINQ to Mocks)
+            ITarget target2 = Mock.Of<ITarget>(o =>
+                o.StrProp == "test" && o.Add(2, 3) == 5
+            );
+
+            // テスト
+            Assert.Equal("test", target1.StrProp);
+            Assert.Equal(5, target1.Add(2, 3));
+            Assert.Equal("test", target2.StrProp);
+            Assert.Equal(5, target2.Add(2, 3));
+
+            // (参考)
+            // モック化対象オブジェクトからモックオブジェクトの取得
+            Mock<ITarget> target2Mock = Mock.Get(target2);
+            target2Mock.Setup(o => o.Add(1, 2)).Returns(100);
+
+            // テスト
+            Assert.Equal("test", target2.StrProp);
+            Assert.Equal(5, target2.Add(2, 3));
+            Assert.Equal(100, target2.Add(1, 2));
+        }
+
+        [Fact(DisplayName = "プロパティ操作の検証")]
+        public void AssertProperty()
+        {
+            var targetMock = new Mock<ITarget>();
+            ITarget target = targetMock.Object;
+
+            targetMock.SetupProperty(o => o.StrProp);
+
+            target.StrProp = "test";
+            Assert.Equal("test", target.StrProp);
+        }
+
+        [Fact(DisplayName = "プロパティ操作の一括検証")]
+        public void VerifyPropertiesAtOnce()
+        {
+            var targetMock = new Mock<ITarget>();
+            ITarget target = targetMock.Object;
+
+            // 一括検証の対象とするためにVerifiable()を指定
+            targetMock
+                .SetupGet(o => o.StrProp)
+                .Verifiable();
+            targetMock
+                .SetupSet(o => o.IntProp = 100) // 検証時の期待値(初期値設定ではない！)
+                .Verifiable();
+
+            _ = target.StrProp;
+            target.IntProp = 1;
+            target.IntProp = 100;
+            target.IntProp = 100;
+            target.IntProp = 200;
+
+            targetMock.Verify(); // 一括検証(検証対象を纏めて検証)
+
+            // 値を保持していないので、最終的な値を検証できない。
+            //Assert.Equal(200, target.IntProp); // NG
+            Assert.Equal(0, target.IntProp);
+        }
+
+        [Fact(DisplayName = "プロパティ操作の個別検証")]
+        public void VerifyProperties()
+        {
+            var targetMock = new Mock<ITarget>();
+            ITarget target = targetMock.Object;
+
+            _ = target.StrProp;
+            target.IntProp = 1;
+            target.IntProp = 100;
+            target.IntProp = 100;
+            target.IntProp = 200;
+
+            targetMock.VerifyGet(o => o.StrProp); // 1回のみ(既定)
+            targetMock.VerifySet(o => o.IntProp = 100, Times.Exactly(2)); // 2回
+            targetMock.VerifySet(o => o.IntProp = 300, Times.Never); // 0回
+
+            // 値を保持していないので、最終的な値を検証できない。
+            //Assert.Equal(200, target.IntProp); // NG
+            Assert.Equal(0, target.IntProp);
+        }
+
+        [Fact(DisplayName = "VerifySet()の検証動作")]
+        public void VerifySetVariation()
+        {
+            var targetMock = new Mock<ITarget>();
+            ITarget target = targetMock.Object;
+
+            target.IntProp = 100;
+            target.IntProp = 101;
+            target.IntProp = 101;
+            target.IntProp = 102;
+            target.IntProp = 102;
+            target.IntProp = 102; // 最終値
+
+            // プロパティを個別に検証
+            targetMock.VerifySet(o => o.IntProp = 100);                    // 1回のみ
+            targetMock.VerifySet(o => o.IntProp = 100, Times.Once);        // 1回のみ
+            targetMock.VerifySet(o => o.IntProp = 101, Times.AtLeastOnce); // 1回以上
+            targetMock.VerifySet(o => o.IntProp = 102, Times.AtLeast(2));  // 2回以上
+            targetMock.VerifySet(o => o.IntProp = 102, Times.Exactly(3));  // 3回
+            targetMock.VerifySet(o => o.IntProp = 999, Times.Never);       // 0回
+
+            // 値を保持していないので、最終的な値を検証できない。
+            //Assert.Equal(102, target.IntProp); // NG
+            Assert.Equal(0, target.IntProp);
+        }
     }
 
     // サンプルで汎用的に使用するテスト用クラス
     public interface ITarget
     {
+        public string StrProp { get; set; }
+        public string ReadonlyStrProp { get; }
+        public int IntProp { get; set; }
+        public int ReadonlyIntProp { get; }
+        public bool BoolProp { get; set; }
+        public bool ReadonlyBoolProp { get; }
+
         public int Add(int x, int y);
         public Task<int> AddAsync(int x, int y);
         public bool Send(string message);
